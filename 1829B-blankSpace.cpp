1f62840739583f0a3cf738fdfa3be6c9696d2222
Asad Khan <167069774+asadiv@users.noreply.github.com>
2025-11-18 08:34:28 +0500

    DAy 10: 8 to 8:10

---
+++ 1829B-blankSpace.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t,n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        vector<int> v;
+        int maxzeros = 0,currentZeros=0;
+        for(int i=0;i<n;i++){
+            int temp;
+            cin>>temp;
+            if(temp==0){
+                currentZeros++;
+                if(currentZeros>maxzeros){
+                    maxzeros = currentZeros;
+                }
+            }else if(temp!=0){
+                currentZeros=0;
+            }
+            
+        }
+
+        cout<<maxzeros<<endl;
+    }
+
+    return 0;
+}
