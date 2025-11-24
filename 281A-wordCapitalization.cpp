d3d0d57734a7fcd67a79675e5dded4d3898cd589
Asad Khan <167069774+asadiv@users.noreply.github.com>
2025-11-24 23:22:52 +0500

    DAy15: 1120pm

---
+++ 281A-wordCapitalization.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string word;
+    cin>>word;
+    word[0] = toupper(word[0]);
+    cout<<word<<endl;
+
+    return 0;
+}
